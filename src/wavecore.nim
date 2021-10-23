20761a6897d2937c0dd8ec0ffcf9414e93eb72b2
oakes <zach@sekao.net>
2021-10-23 15:47:39 +0200

    Make second level of replies

--- src/wavecore.nim
+++ src/wavecore.nim
@@ -56,7 +56,7 @@ when isMainModule:
   for i in 1..500:
     var p2 = entities.Post(parent_id: p1.id, user_id: bob.id, body: db.CompressedValue(uncompressed: "Hello, i'm bob"))
     p2.id = server.insertPost(s, p2)
-    var p3 = entities.Post(parent_id: p1.id, user_id: alice.id, body: db.CompressedValue(uncompressed: "What's up"))
+    var p3 = entities.Post(parent_id: p2.id, user_id: alice.id, body: db.CompressedValue(uncompressed: "What's up"))
     p3.id = server.insertPost(s, p3)
   discard readLine(stdin)
   server.stop(s)
