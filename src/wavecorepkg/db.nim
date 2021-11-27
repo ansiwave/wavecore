98867ef893822790da933ae8a02b625f7cc20188
oakes <zach@sekao.net>
2021-11-27 22:44:43 +0100

    Init db in a transaction

--- src/wavecorepkg/db.nim
+++ src/wavecorepkg/db.nim
@@ -5,53 +5,6 @@ from ./db/db_sqlite import sql
 from bitops import nil
 import tables
 
-const
-  SQLITE_OPEN_READONLY = 1
-  SQLITE_OPEN_READWRITE = 2
-  SQLITE_OPEN_CREATE = 4
-
-proc sqlite3_open_v2(filename: cstring, ppDb: var PSqlite3, flags: cint, zVfs: cstring): cint {.cdecl, importc.}
-
-proc open*(filename: string, http: bool = false): PSqlite3 =
-  let
-    flags: cint = if http: SQLITE_OPEN_READONLY else: bitops.bitor(SQLITE_OPEN_READWRITE, SQLITE_OPEN_CREATE)
-    vfs: cstring = if http: "http".cstring else: "multiplex".cstring
-  if sqlite3_open_v2(filename, result, flags, vfs) != SQLITE_OK:
-    db_sqlite.dbError(result)
-
-proc init*(conn: PSqlite3) =
-  db_sqlite.exec conn, sql"""
-    CREATE TABLE user (
-      user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-      ts INTEGER,
-      public_key TEXT UNIQUE,
-      public_key_algo TEXT
-    ) STRICT
-  """
-  db_sqlite.exec conn, sql"CREATE INDEX user_public_key ON user(public_key)"
-  db_sqlite.exec conn, sql"CREATE VIRTUAL TABLE user_search USING fts5 (user_id, attribute, value, value_unindexed UNINDEXED)"
-  db_sqlite.exec conn, sql"""
-    CREATE TABLE post (
-      post_id INTEGER PRIMARY KEY AUTOINCREMENT,
-      ts INTEGER,
-      content BLOB,
-      content_sig TEXT UNIQUE,
-      content_sig_last TEXT UNIQUE,
-      public_key TEXT,
-      parent TEXT,
-      parent_public_key TEXT,
-      reply_count INTEGER,
-      score INTEGER
-    ) STRICT
-  """
-  db_sqlite.exec conn, sql"CREATE INDEX post_content_sig ON post(content_sig)"
-  db_sqlite.exec conn, sql"CREATE INDEX post_content_sig_last ON post(content_sig_last)"
-  db_sqlite.exec conn, sql"CREATE INDEX post_public_key ON post(public_key)"
-  db_sqlite.exec conn, sql"CREATE INDEX post_parent ON post(parent)"
-  db_sqlite.exec conn, sql"CREATE INDEX post_parent_public_key ON post(parent_public_key)"
-  db_sqlite.exec conn, sql"CREATE INDEX post_parent_score ON post(parent, score)"
-  db_sqlite.exec conn, sql"CREATE VIRTUAL TABLE post_search USING fts5 (post_id, user_id, attribute, value, value_unindexed UNINDEXED)"
-
 template withStatement*(conn: PSqlite3, query: string, stmt: PStmt, body: untyped) =
   try:
     if prepare_v2(conn, query, query.len.cint, stmt, nil) != SQLITE_OK:
@@ -67,6 +20,54 @@ template withTransaction*(conn: PSqlite3, body: untyped) =
     body
   finally:
     db_sqlite.exec(conn, sql"COMMIT")
+
+const
+  SQLITE_OPEN_READONLY = 1
+  SQLITE_OPEN_READWRITE = 2
+  SQLITE_OPEN_CREATE = 4
+
+proc sqlite3_open_v2(filename: cstring, ppDb: var PSqlite3, flags: cint, zVfs: cstring): cint {.cdecl, importc.}
+
+proc open*(filename: string, http: bool = false): PSqlite3 =
+  let
+    flags: cint = if http: SQLITE_OPEN_READONLY else: bitops.bitor(SQLITE_OPEN_READWRITE, SQLITE_OPEN_CREATE)
+    vfs: cstring = if http: "http".cstring else: "multiplex".cstring
+  if sqlite3_open_v2(filename, result, flags, vfs) != SQLITE_OK:
+    db_sqlite.dbError(result)
+
+proc init*(conn: PSqlite3) =
+  withTransaction(conn):
+    db_sqlite.exec conn, sql"""
+      CREATE TABLE user (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ts INTEGER,
+        public_key TEXT UNIQUE,
+        public_key_algo TEXT
+      ) STRICT
+    """
+    db_sqlite.exec conn, sql"CREATE INDEX user_public_key ON user(public_key)"
+    db_sqlite.exec conn, sql"CREATE VIRTUAL TABLE user_search USING fts5 (user_id, attribute, value, value_unindexed UNINDEXED)"
+    db_sqlite.exec conn, sql"""
+      CREATE TABLE post (
+        post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ts INTEGER,
+        content BLOB,
+        content_sig TEXT UNIQUE,
+        content_sig_last TEXT UNIQUE,
+        public_key TEXT,
+        parent TEXT,
+        parent_public_key TEXT,
+        reply_count INTEGER,
+        score INTEGER
+      ) STRICT
+    """
+    db_sqlite.exec conn, sql"CREATE INDEX post_content_sig ON post(content_sig)"
+    db_sqlite.exec conn, sql"CREATE INDEX post_content_sig_last ON post(content_sig_last)"
+    db_sqlite.exec conn, sql"CREATE INDEX post_public_key ON post(public_key)"
+    db_sqlite.exec conn, sql"CREATE INDEX post_parent ON post(parent)"
+    db_sqlite.exec conn, sql"CREATE INDEX post_parent_public_key ON post(parent_public_key)"
+    db_sqlite.exec conn, sql"CREATE INDEX post_parent_score ON post(parent, score)"
+    db_sqlite.exec conn, sql"CREATE VIRTUAL TABLE post_search USING fts5 (post_id, user_id, attribute, value, value_unindexed UNINDEXED)"
 
 proc select*[T](conn: PSqlite3, init: proc (stmt: PStmt): T, query: string, args: varargs[string, `$`]): seq[T] =
   var stmt: PStmt
