fe930fea5cab774940edcfc938385f389856de67
oakes <zach@sekao.net>
2021-12-14 13:50:02 +0100

    Quit if root directory isn't found

--- src/wavecore.nim
+++ src/wavecore.nim
@@ -2,10 +2,13 @@ from ./wavecorepkg/testrun import nil
 from ./wavecorepkg/server import nil
 from ./wavecorepkg/db/vfs import nil
 from ./wavecorepkg/paths import nil
+from os import nil
 
 const port = 3000
 
 when isMainModule:
+  if not os.dirExists(paths.staticFileDir):
+    quit "Can't find directory: " & paths.staticFileDir
   vfs.register()
   var s = server.initServer("localhost", port, paths.staticFileDir)
   server.start(s)
