f4d91b86e30d41414eb8f304da6c97397c1a493a
oakes <zach@sekao.net>
2022-04-25 06:42:35 +0200

    Upgrade limbo

--- src/wavecore.nim
+++ src/wavecore.nim
@@ -25,16 +25,24 @@ when isMainModule:
 
   if not os.dirExists(paths.staticFileDir):
     quit "Can't find directory: " & paths.staticFileDir
+
+  proc upgradeBoard(board: string) =
+    os.createDir(board / "ansiwave")
+    for (kind, path) in os.walkDir(board / "ansiwavez"):
+      let (dir, name, ext) = os.splitFile(path)
+      if kind == os.pcFile and ext == ".ansiwavez":
+        writeFile(board / "ansiwave" / name & ".ansiwave", zippy.uncompress(readFile(path), dataFormat = zippy.dfZlib))
+        os.removeFile(path)
+    os.removeDir(board / "ansiwavez")
+
   for (kind, board) in os.walkDir(paths.staticFileDir / "boards"):
     echo "Upgrading ", board
-    if kind == os.pcDir and os.dirExists(board / "ansiwavez"):
-      os.createDir(board / "ansiwave")
-      for (kind, path) in os.walkDir(board / "ansiwavez"):
-        let (dir, name, ext) = os.splitFile(path)
-        if kind == os.pcFile and ext == ".ansiwavez":
-          writeFile(board / "ansiwave" / name & ".ansiwave", zippy.uncompress(readFile(path), dataFormat = zippy.dfZlib))
-          os.removeFile(path)
-      os.removeDir(board / "ansiwavez")
+    if kind == os.pcDir:
+      if os.dirExists(board / "ansiwavez"):
+        upgradeBoard(board)
+      if os.dirExists(board / "misc" / "limbo"):
+        for (kind, board) in os.walkDir(board / "misc" / "limbo"):
+          upgradeBoard(board)
 
   vfs.register()
   var s = server.initServer("localhost", port, paths.staticFileDir, options)
